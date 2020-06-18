26300c55b13a5d93e3d5bede2e58d7729a312581
Justin Breneman <justin.breneman@yahoo.com>
2020-06-18 20:59:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,15 @@ class Artist
   attr_accessor :name
   def initialize(name)
     @name = name
-#    @songs = []
   end
   def songs
     Song.all.select {|song| song.artist == self}
   end
   def add_song(song)
     song.artist = self
+  end
+  def add_song_by_name(song)
+    Song.new(song)
+    add_song(song)
   end
 end
