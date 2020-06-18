356db3ac66212a4f341f4c435f769bbe1a386c1e
Justin Breneman <justin.breneman@yahoo.com>
2020-06-18 21:12:33 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+    @name = name
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
